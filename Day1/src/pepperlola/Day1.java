1c940f104d2a14479d7d90531916c5c5e255538e
PepperLola <julian.orion.wright@gmail.com>
2020-12-01 09:13:58 -0800

    finished day 1

--- Day1/src/pepperlola/Day1.java
+++ Day1/src/pepperlola/Day1.java
@@ -3,18 +3,36 @@ package pepperlola;
 import java.util.List;
 
 public class Day1 {
-    public static void main(String[] args) {
-        String directory = System.getProperty("user.dir");
-        List<String> input = FileUtil.loadFile(directory + "/src/pepperlola/input1.txt");
-        for (String in1 : input) {
+    public static int part1(List<String> input) {
+        for (String in1 : input)
             for (String in2 : input) {
                 int n1 = Integer.parseInt(in1);
                 int n2 = Integer.parseInt(in2);
 
                 if (n1 + n2 == 2020) {
-                    System.out.println(n1 * n2);
+                    return (n1 * n2);
                 }
             }
-        }
+        return 0;
+    }
+
+public static int part2(List<String> input) {
+        for (String in1 : input) 
+            for (String in2 : input)
+                for (String in3 : input) {
+                    int n1 = Integer.parseInt(in1);
+                    int n2 = Integer.parseInt(in2);
+                    int n3 = Integer.parseInt(in3);
+                    if (n1 + n2 + n3 == 2020)
+                        return n1 * n2 * n3;
+                }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String directory = System.getProperty("user.dir");
+        List<String> input = FileUtil.loadFile(directory + "/src/pepperlola/input1.txt");
+        System.out.println(part1(input));
+        System.out.println(part2(input));
     }
 }
