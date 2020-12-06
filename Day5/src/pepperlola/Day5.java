d2345d7f07a7e307a4a0fa742acbdd5cac4dce84
PepperLola <julian.orion.wright@gmail.com>
2020-12-06 00:23:02 -0800

    cleaned up day 5, made it fewer lines although some of part 2 is dumb and on one line and it doesn't make much sense just by looking at it for a second

--- Day5/src/pepperlola/Day5.java
+++ Day5/src/pepperlola/Day5.java
@@ -2,51 +2,16 @@ package pepperlola;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day5 {
 
     public static long part1(List<String> input) {
         int highestId = 0;
         for (String line : input) {
-            int low = 0;
-            int high = 127;
-            int row = 0;
-
-            char[] chars = line.toCharArray();
-            for (int i = 0; i < chars.length - 4; i++) {
-                char character = chars[i];
-
-                if (character == 'B') {
-                    low = (low + high) / 2 + 1;
-                } else if (character == 'F') {
-                    high = (low + high) / 2;
-                }
-            }
-            if (chars[chars.length - 4] == 'B') {
-                row = high;
-            } else if (chars[chars.length - 4] == 'F') {
-                row = low;
-            }
-            int column = 0;
-            low = 0;
-            high = 7;
-            for (int i = chars.length - 3; i < chars.length - 1; i++) {
-                char character = chars[i];
-
-                if (character == 'R') {
-                    low = (low + high) / 2 + 1;
-                } else if (character == 'L') {
-                    high = (low + high) / 2;
-                }
-            }
-            if (chars[chars.length - 1] == 'R') {
-                column = high;
-            } else if (chars[chars.length - 1] == 'L') {
-                column = low;
-            }
-
-            int seatId = row * 8 + column;
+            int seatId = getSeatId(line);
             if (seatId > highestId)
                 highestId = seatId;
         }
@@ -54,57 +19,11 @@ public class Day5 {
     }
 
     public static long part2(List<String> input) {
-        int highestId = 0;
-        List<Integer> idList = new ArrayList<>();
-        for (String line : input) {
-            int low = 0;
-            int high = 127;
-            int row = 0;
 
-            char[] chars = line.toCharArray();
-            for (int i = 0; i < chars.length - 4; i++) {
-                char character = chars[i];
+        Integer[] array = input.stream().map(Day5::getSeatId).sorted().toArray(Integer[]::new);
 
-                if (character == 'B') {
-                    low = (low + high) / 2 + 1;
-                } else if (character == 'F') {
-                    high = (low + high) / 2;
-                }
-            }
-            if (chars[chars.length - 4] == 'B') {
-                row = high;
-            } else if (chars[chars.length - 4] == 'F') {
-                row = low;
-            }
-            int column = 0;
-            low = 0;
-            high = 7;
-            for (int i = chars.length - 3; i < chars.length - 1; i++) {
-                char character = chars[i];
+        int low = 0, high = array.length - 1, index = 0;
 
-                if (character == 'R') {
-                    low = (low + high) / 2 + 1;
-                } else if (character == 'L') {
-                    high = (low + high) / 2;
-                }
-            }
-            if (chars[chars.length - 1] == 'R') {
-                column = high;
-            } else if (chars[chars.length - 1] == 'L') {
-                column = low;
-            }
-
-            int seatId = row * 8 + column;
-            idList.add(seatId);
-        }
-
-        Integer[] array = idList.toArray(new Integer[0]);
-        Arrays.sort(array);
-
-        int low = 0;
-        int high = array.length - 1;
-
-        int index = 0;
         while (low < high) {
             index = (low + high) / 2;
             if (array[index] == array[low] + (index - low)) {
@@ -115,6 +34,47 @@ public class Day5 {
         }
 
         return array[index] + 1;
+    }
+
+    public static int getSeatId(String line) {
+        int low = 0;
+        int high = 127;
+        int row = 0;
+
+        char[] chars = line.toCharArray();
+        for (int i = 0; i < chars.length - 4; i++) {
+            char character = chars[i];
+
+            if (character == 'B') {
+                low = (low + high) / 2 + 1;
+            } else if (character == 'F') {
+                high = (low + high) / 2;
+            }
+        }
+        if (chars[chars.length - 4] == 'B') {
+            row = high;
+        } else if (chars[chars.length - 4] == 'F') {
+            row = low;
+        }
+        int column = 0;
+        low = 0;
+        high = 7;
+        for (int i = chars.length - 3; i < chars.length - 1; i++) {
+            char character = chars[i];
+
+            if (character == 'R') {
+                low = (low + high) / 2 + 1;
+            } else if (character == 'L') {
+                high = (low + high) / 2;
+            }
+        }
+        if (chars[chars.length - 1] == 'R') {
+            column = high;
+        } else if (chars[chars.length - 1] == 'L') {
+            column = low;
+        }
+
+        return row * 8 + column;
     }
 
     public static void main(String[] args) {
